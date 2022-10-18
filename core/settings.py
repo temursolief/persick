14cc8fdd965145fdf6790413a5ee4b54db40e5bf
temursolief <temursoliev2110@gmail.com>
2022-10-18 20:00:30 +0500

    added env_example and secured the keys

--- core/settings.py
+++ core/settings.py
@@ -1,14 +1,17 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+dotenv_file = BASE_DIR / '.env'
+if os.path.isfile(dotenv_file):
+    dotenv.load(dotenv_file)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure---_e0uw=e70go^-jnt28p#+!ojswafo8dn=okd&ifk3y0t0qtj'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
