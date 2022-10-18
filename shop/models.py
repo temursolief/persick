2844c8f91a82a67a2f58b200891c63b4d043d8fb
temursolief <temursoliev2110@gmail.com>
2022-10-18 17:38:12 +0500

    created product model

--- shop/models.py
+++ shop/models.py
@@ -11,3 +11,34 @@ class Category(models.Model):
         ]
         verbose_name = "category"
         verbose_name_plural = "categories" 
+
+    def __str__(self):
+        return self.name
+
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, 
+            on_delete=models.CASCADE, 
+            related_name="products")
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=256)
+    image = models.ImageField(upload_to="products/%Y/%m/%d",
+            blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+            models.Index(fields=['price'])
+        ]
+    
+    def __str__(self):
+        return self.name
