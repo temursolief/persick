51be03e6ca5e8d928ee16466f353d16814b4be6a
temursolief <temursoliev2110@gmail.com>
2022-10-18 20:12:42 +0500

    fixed the bugs in list.html, views

--- shop/views.py
+++ shop/views.py
@@ -9,7 +9,7 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category,
             slug=category_slug)
-        products = products.objects.filter(category=category)
+        products = products.filter(category=category)
     return render(request, 
             'shop/product/list.html',
             {'category': category,
