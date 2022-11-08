b30dae7dd303e0b576addec2afe80ed31c006a8a
temursolief <temursoliev2110@gmail.com>
2022-11-08 18:08:16 +0500

    added order_pdf to OrderAdmin model

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import order_create, admin_order_detail
+from .views import order_create, admin_order_detail, admin_export_pdf
 
 app_name = 'orders'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', order_create, name='order_create'),
     path('admin/order/<int:order_id>/', admin_order_detail, 
         name='admin_order_detail'),
+    path('admin/order/<int:order_id>/pdf/', admin_export_pdf, name='admin_order_pdf')
 ]
