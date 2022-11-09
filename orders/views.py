ae59322f13a4c06a61f551a9a902b169ff98a56d
temursolief <temursoliev2110@gmail.com>
2022-11-09 23:15:47 +0500

    created a coupon model

--- orders/views.py
+++ orders/views.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 from django.conf import settings
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-import weasyprint
+# import weasyprint
 
 from .models import OrderItem, Order
 from .forms import OrderCreateForm
@@ -40,10 +40,11 @@ def admin_order_detail(request, order_id):
 
 @staff_member_required
 def admin_export_pdf(request, order_id):
-    order = get_object_or_404(Order, id=order_id)
-    html = render_to_string('orders/order/pdf.html', {'order': order})
-    response = HttpResponse(content_type='application/pdf')
-    response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
-    weasyprint.HTML(string=html).write_pdf(response,
-        stylesheets=[weasyprint.CSS(settings.STATIC_ROOT/'css/pdf.css')])
-    return response
+    # order = get_object_or_404(Order, id=order_id)
+    # html = render_to_string('orders/order/pdf.html', {'order': order})
+    # response = HttpResponse(content_type='application/pdf')
+    # response['Content-Disposition'] = f'filename=order_{order.id}.pdf'
+    # weasyprint.HTML(string=html).write_pdf(response,
+    #     stylesheets=[weasyprint.CSS(settings.STATIC_ROOT/'css/pdf.css')])
+    # return response
+    pass
--- payment/tasks.py
+++ payment/tasks.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 from celery import shared_task
-import weasyprint
+# import weasyprint
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
@@ -19,7 +19,7 @@ def payment_completed(order_id):
 
     html = render_to_string('orders/order/pdf.html', {'order': order})
     out = BytesIO()
-    stylesheets = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
-    weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
+    # stylesheets = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
+    # weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
     email.attach(f'order_{order.id}.pdf', out.getvalue(), 'application/pdf')
     email.send()
